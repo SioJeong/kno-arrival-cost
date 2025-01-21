4e645aa98dc0c3a61ae567b687068466e990b294
jeongsangyoung <jeongsangyoung@naver.com>
2025-01-21 18:31:32 +0900

    feat: 평일, 주말 및 오전 11시 구분 로직 구현

--- app/api/exchange-rate/route.ts
+++ app/api/exchange-rate/route.ts
@@ -1,18 +1,27 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 import { NextResponse } from 'next/server';
 
-export async function GET() {
-    const apiKey = process.env.EXCHANGERATE_API_KEY;
+export async function GET(request: Request) {
+    const API_KEY = process.env.EXCHANGERATE_API_KEY;
+    const { searchParams } = new URL(request.url);
+    const searchdate = searchParams.get('searchdate');
 
-    if (!apiKey) {
+    if (!API_KEY) {
         return NextResponse.json({ error: 'API key is missing' }, { status: 500 });
     }
 
-    // TODO: 주말, 평일 11시 이전, 공휴일은 직전 영업일 고시 환율로 표시
-    // const url = `https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=${apiKey}&searchdate=${today}&data=AP01`;
-    const url = `https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=${apiKey}&data=AP01`;
+    if (!searchdate) {
+        return NextResponse.json({ error: 'Search date is required' }, { status: 400 });
+    }
+
+    const url = `https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=${API_KEY}&searchdate=${searchdate}&data=AP01`;
+
+    console.log(url);
 
     try {
         const response = await fetch(url);
+        console.log(response.status);
 
         if (!response.ok) {
             throw new Error(`Failed to fetch exchange rates: ${response.status}`);
@@ -20,14 +29,13 @@ export async function GET() {
 
         const data = await response.json();
 
-        // 유로(EUR)와 미국 달러(USD)만 필터링
         const filteredData = data.filter((rate: any) => ['EUR', 'USD'].includes(rate.cur_unit));
 
         const res = NextResponse.json(filteredData);
-        res.headers.set('Cache-Control', 'public, s-maxage=86400, stale-while-revalidate=3600'); // 하루 동안 캐싱, 만료 후 1시간 동안 재검증
+        res.headers.set('Cache-Control', 'public, s-maxage=43200, stale-while-revalidate=86400');
+
         return res;
     } catch (error: any) {
-        console.error('Error fetching exchange rates:', error);
         return NextResponse.json({ error: error.message }, { status: 500 });
     }
 }
