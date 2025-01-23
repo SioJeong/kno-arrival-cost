4f7858a3e2278f7dbd5609a1a47fc312ea8f1538
jeongsangyoung <jeongsangyoung@naver.com>
2025-01-23 23:12:34 +0900

    fix: api 호출 에러 node-fetch 적용

--- app/actions/fetchExchangeRates.ts
+++ app/actions/fetchExchangeRates.ts
@@ -1,5 +1,6 @@
 'use server';
 
+import fetch from 'node-fetch';
 import { calculateValidDate } from '../utils/calculateValidDate';
 
 export async function fetchExchangeRates() {
@@ -8,14 +9,17 @@ export async function fetchExchangeRates() {
         const API_KEY = process.env.EXCHANGERATE_API_KEY;
 
         const response = await fetch(
-            `https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=${API_KEY}&searchdate=${searchDate}&data=AP01`
+            `https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=${API_KEY}&searchdate=${searchDate}&data=AP01`,
+            {
+                agent: new (require('https').Agent)({ rejectUnauthorized: false }),
+            }
         );
 
         if (!response.ok) {
             throw new Error('Failed to fetch exchange rates');
         }
 
-        const data = await response.json();
+        const data = (await response.json()) as ExchangeRate[];
 
         return {
             rates: data.filter((rate: ExchangeRate) => ['EUR', 'USD'].includes(rate.cur_unit)),
